d4985de46de03a52acf1fc8b351a3fe5b667d28a
Muhamad Ilham Fauzan <ilhamfauzan41@gmail.com>
2017-12-14 16:13:13 +0700

    Create host & add override function OnStartHost

---
+++ Assets/_Script/Network/MyNetworkManager.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class MyNetworkManager : NetworkManager {
+
+    public void MyStartHost() {
+        StartHost();
+    }
+
+    public override void OnStartHost() {
+        print("When host started what to do");
+    }
+
+}
